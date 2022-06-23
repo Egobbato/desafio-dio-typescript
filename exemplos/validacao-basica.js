089e2a029d43497bf4b0178f7d4ad1217b7ca910
Eduardo Gobbato <102391940+Egobbato@users.noreply.github.com>
2022-06-23 09:12:50 -0300

    first commit

---
+++ exemplos/validacao-basica.js
@@ -0,0 +1,15 @@
+// let button = document.getElementById("button");
+// let input1 = document.getElementById("input1");
+// let input2 = document.getElementById("input2");
+
+// function somarNumeros(num1, num2) {
+//   if (typeof num1 === "number" && typeof num2 === "number") {
+//     return num1 + num2;
+//   } else {
+//     return Number(num1) + Number(num2);
+//   }
+// }
+
+// button.addEventListener("click", () => {
+//   console.log(somarNumeros(input1.value, input2.value));
+// });
